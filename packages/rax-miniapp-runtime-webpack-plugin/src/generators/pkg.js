a8f421028536ed24e75ec659e8803002a5963079
督布 <dengce.dc@alibaba-inc.com>
2020-11-09 10:55:53 +0800

    fix: 修改绝对拼接为相对路径

--- packages/rax-miniapp-runtime-webpack-plugin/src/generators/pkg.js
+++ packages/rax-miniapp-runtime-webpack-plugin/src/generators/pkg.js
@@ -1,16 +1,16 @@
-const { resolve } = require('path');
-const { readJSONSync } = require('fs-extra');
-const addFileToCompilation = require('../utils/addFileToCompilation');
+const { resolve } = require("path");
+const { readJSONSync } = require("fs-extra");
+const addFileToCompilation = require("../utils/addFileToCompilation");
 
-module.exports = function(compilation, { target, command, rootDir }) {
-  const pkgPath = resolve(process.cwd(), 'package.json');
+module.exports = function (compilation, { target, command, rootDir }) {
+  const pkgPath = resolve(process.cwd(), "package.json");
   const pkgContent = readJSONSync(pkgPath, {
-    encoding: 'utf-8'
+    encoding: "utf-8",
   });
   addFileToCompilation(compilation, {
-    filename: 'package.json',
+    filename: "package.json",
     content: JSON.stringify({
-      dependencies: pkgContent.dependencies
+      dependencies: pkgContent.dependencies,
     }),
     command,
     target,
--- packages/rax-miniapp-runtime-webpack-plugin/src/generators/render.js
+++ packages/rax-miniapp-runtime-webpack-plugin/src/generators/render.js
@@ -1,20 +1,17 @@
-const { resolve } = require('path');
-const { readFileSync } = require('fs-extra');
-const adapter = require('../adapter');
-const addFileToCompilation = require('../utils/addFileToCompilation');
+const { resolve } = require("path");
+const { readFileSync } = require("fs-extra");
+const adapter = require("../adapter");
+const addFileToCompilation = require("../utils/addFileToCompilation");
 
-module.exports = function(compilation, { target, command, rootDir }) {
-  const sourceNpmFile = resolve(
-    process.cwd(),
-    'node_modules',
-    'miniapp-render',
-    'dist',
-    adapter[target].fileName,
-    command === 'build' ? 'index.min.js' : 'index.js'
-  );
+module.exports = function (compilation, { target, command, rootDir }) {
+  const sourceNpmFile = require
+    .resolve("miniapp-render")
+    .replace("ali", adapter[target].fileName)
+    .replace("index.js", command === "build" ? "index.min.js" : "index.js");
+
   addFileToCompilation(compilation, {
-    filename: 'render.js',
-    content: readFileSync(sourceNpmFile, 'utf-8'),
+    filename: "render.js",
+    content: readFileSync(sourceNpmFile, "utf-8"),
     command,
     target,
   });
